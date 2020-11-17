19e61cfa2d90987d4986185771ffc338971c431b
于志程 <ycc0740@163.com>
2020-11-17 11:08:46 +0800

    调整路由

---
+++ frontends/src/routes/SubsidenceMonitor/index.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: @yzcheng
+ * @Date: 2020-11-12 10:44:29
+ * @Version: 1.0
+ * @LastEditors: @yzcheng
+ * @Description: 沉降监测
+ * @LastEditTime: 2020-11-17 11:05:27
+ */
+
+import React, { Component } from 'react';
+
+class ContructionProgress extends Component {
+    render() {
+        return (
+            <div className="function-page">
+                这里是沉降监测
+            </div>
+        );
+    }
+}
+
+export default ContructionProgress; 
