fcc3db49846a0eb339b1c29e264c9bc482a02797
于志程 <ycc0740@163.com>
2020-11-17 21:14:04 +0800

    更新代码

--- frontends/src/routes/RiverSandCapa/components/DistributedComputing/index.jsx
+++ frontends/src/routes/RiverSandCapa/components/DistributedComputing/index.jsx
@@ -4,10 +4,34 @@
  * @Version: 1.0
  * @LastEditors: @yzcheng
  * @Description: 分布计算
- * @LastEditTime: 2020-11-17 17:36:40
+ * @LastEditTime: 2020-11-17 21:13:42
  */
 import React from 'react'
-
+import { Row, Col, Steps, Select } from 'antd'
+import StateInformation from './StateInformation'
+const { Step } = Steps
+const { Option } = Select
 export default function index() {
-    return <div>分布计算</div>
+  return (
+    <div>
+      <Row>
+        <Col span={3}>空间分布计算</Col>
+        <Col span={21}>
+          <Select style={{ width: 120 }}>
+            <Option value="1">项目1</Option>
+          </Select>
+        </Col>
+        <Col span={3}>
+          <Steps direction="vertical" size="default" current={1}>
+            <Step />
+            <Step />
+            <Step />
+          </Steps>
+        </Col>
+        <Col span={21}>
+          <StateInformation />
+        </Col>
+      </Row>
+    </div>
+  )
 }
