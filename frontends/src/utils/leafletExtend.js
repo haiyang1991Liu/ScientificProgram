668449879ec5c58d30b2ccb827fc3c717df42672
于志程 <ycc0740@163.com>
2020-11-19 18:49:22 +0800

    更新对接接口

---
+++ frontends/src/utils/leafletExtend.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: @yzcheng
+ * @Date: 2020-11-19 18:44:58
+ * @Version: 1.0
+ * @LastEditors: @yzcheng
+ * @Description: 
+ * @LastEditTime: 2020-11-19 18:47:27
+ */
+import L from 'leaflet'
+export function extendLeaflet() {
+    
+}
